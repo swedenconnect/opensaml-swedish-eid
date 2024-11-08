ef4e9addc958deefd371d79cf83153cb069abdd9
Martin Lindström <martin-lindstrom@users.noreply.github.com>
2024-11-08 15:14:28 +0100

    2.2.2 (#49)
    2.2.2 release - only dependency updates.

--- src/main/java/se/swedenconnect/opensaml/sweid/LibraryVersion.java
+++ src/main/java/se/swedenconnect/opensaml/sweid/LibraryVersion.java
@@ -24,12 +24,12 @@ public final class LibraryVersion {
 
   private static final int MAJOR = 2;
   private static final int MINOR = 2;
-  private static final int PATCH = 1;
+  private static final int PATCH = 2;
 
   /**
    * Global serialization value for library classes.
    */
-  public static final long SERIAL_VERSION_UID = getVersion().hashCode();
+  public static final long SERIAL_VERSION_UID = (MAJOR + "." + MINOR).hashCode();
 
   /**
    * Gets the version string.
